3a0717e9ef40a6cb541eaf4644c3c3e3bfda3fd1
Sunny Singh <sunny@sunnyis.me>
2016-07-29 00:55:04 -0400

    Update release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,17 +1,13 @@
 #! /usr/bin/env node
-var prompt = require('prompt');
+const prompt = require('prompt');
 
 prompt.start();
 
 echo('What type of version release is this (major, minor, patch, or new version number)?');
 prompt.get(['version'], function (error, result) {
-  var version = result.version;
 
-  var newVersion = exec('npm version '+version, {silent: true}).stdout.replace(/(\r\n|\n|\r)/gm, '');
+  exec('npm version '+result.version);
 
-  echo('Pushing to GitHub');
-  exec('git push && git push --tags');
-
-  echo('\r\nsocialshares '+newVersion+' released!');
+  echo('\r\nsocialshares released!');
 
 });
