99b81b832e5ca2fadc30cbe8c22d515b01f759f2
Sunny Singh <sunny@sunnyis.me>
2016-08-05 21:40:21 -0400

    Fix coding style in npm scripts

--- scripts/create-dist.js
+++ scripts/create-dist.js
@@ -4,72 +4,72 @@
  * Cleanup / Optimize
  */
 
-echo('Cleaning up dist directory');
-rm('-rf', 'dist');
-mkdir('dist');
+echo('Cleaning up dist directory')
+rm('-rf', 'dist')
+mkdir('dist')
 
-echo('Optimizing SVG icons');
-exec('npm run svgo');
+echo('Optimizing SVG icons')
+exec('npm run svgo')
 
 /**
  * Build Socialshares: With Icons
  */
 
-echo('Creating socialshares.js');
-exec('npm run build');
+echo('Creating socialshares.js')
+exec('npm run build')
 
-echo('Copying socialshares.js and socialshares.js.map to dist');
-cp('build/socialshares.js', 'dist/socialshares.js');
-cp('build/socialshares.js.map', 'dist/socialshares.js.map');
-sed('-i', '{VERSION}', env.npm_package_version, 'dist/socialshares.js');
+echo('Copying socialshares.js and socialshares.js.map to dist')
+cp('build/socialshares.js', 'dist/socialshares.js')
+cp('build/socialshares.js.map', 'dist/socialshares.js.map')
+sed('-i', '{VERSION}', env.npm_package_version, 'dist/socialshares.js')
 
-echo('Creating socialshares.min.js');
-exec('npm run build-min');
+echo('Creating socialshares.min.js')
+exec('npm run build-min')
 
-echo('Copying socialshares.min.js to dist');
-cp('build/socialshares.js', 'dist/socialshares.min.js');
-sed('-i', '{VERSION}', env.npm_package_version, 'dist/socialshares.min.js');
+echo('Copying socialshares.min.js to dist')
+cp('build/socialshares.js', 'dist/socialshares.min.js')
+sed('-i', '{VERSION}', env.npm_package_version, 'dist/socialshares.min.js')
 
 /**
  * Build Socialshares: Without Icons
  */
 
-echo('Creating socialshares.js without icons');
-exec('npm run build --  --exclude-icons');
+echo('Creating socialshares.js without icons')
+exec('npm run build --  --exclude-icons')
 
-echo('Copying socialshares.js and socialshares.js.map to dist');
-cp('build/socialshares.js', 'dist/socialshares.noicons.js');
-cp('build/socialshares.js.map', 'dist/socialshares.noicons.js.map');
-sed('-i', '{VERSION}', env.npm_package_version, 'dist/socialshares.noicons.js');
-sed('-i', 'socialshares.js.map', 'socialshares.noicons.js.map', 'dist/socialshares.noicons.js');
+echo('Copying socialshares.js and socialshares.js.map to dist')
+cp('build/socialshares.js', 'dist/socialshares.noicons.js')
+cp('build/socialshares.js.map', 'dist/socialshares.noicons.js.map')
+sed('-i', '{VERSION}', env.npm_package_version, 'dist/socialshares.noicons.js')
+sed('-i', 'socialshares.js.map', 'socialshares.noicons.js.map', 'dist/socialshares.noicons.js')
 
-echo('Creating socialshares.min.js without icons');
-exec('npm run build-min --  --exclude-icons');
+echo('Creating socialshares.min.js without icons')
+exec('npm run build-min --  --exclude-icons')
 
-echo('Copying socialshares.min.js to dist');
-cp('build/socialshares.js', 'dist/socialshares.noicons.min.js');
-sed('-i', '{VERSION}', env.npm_package_version, 'dist/socialshares.noicons.min.js');
+echo('Copying socialshares.min.js to dist')
+cp('build/socialshares.js', 'dist/socialshares.noicons.min.js')
+sed('-i', '{VERSION}', env.npm_package_version, 'dist/socialshares.noicons.min.js')
 
 /**
  * Build Icons
  */
 
-echo('Creating icons');
-exec('npm run build-icons');
+echo('Creating icons')
+exec('npm run build-icons')
 
-echo('Copying icons to dist');
-cp('-r', 'build/icons/', 'dist/icons/');
+echo('Copying icons to dist')
+cp('-r', 'build/icons/', 'dist/icons/')
 
 /**
  * HTML Demo
  */
 
-echo('Copying index.html to dist');
-cp('build/index.html', 'dist/index.html');
-sed('-i', './socialshares.js', './socialshares.min.js', 'dist/index.html');
+echo('Copying index.html to dist')
+cp('build/index.html', 'dist/index.html')
+sed('-i', './socialshares.js', './socialshares.min.js', 'dist/index.html')
 
 /**
  * Done
  */
 
-echo('The dist directory is now ready!');
+echo('The dist directory is now ready!')
--- scripts/release.js
+++ scripts/release.js
@@ -1,36 +1,36 @@
 #! /usr/bin/env node
-const inquirer = require('inquirer');
-const colors = require('colors');
+const inquirer = require('inquirer')
+const colors = require('colors')
 
-echo(`socialshares release | current: v${env.npm_package_version}\n`.bold.magenta);
+echo(`socialshares release | current: v${env.npm_package_version}\n`.bold.magenta)
 
 inquirer.prompt([
-    {
-        name: 'version',
-        message: 'What type of version release is this?',
-        type: 'list',
-        choices: ['major', 'minor', 'patch', 'custom'],
-    },
+  {
+    name: 'version',
+    message: 'What type of version release is this?',
+    type: 'list',
+    choices: ['major', 'minor', 'patch', 'custom'],
+  },
 ]).then(answers => {
-    const npmVersion = version => {
-        exec(`npm version ${version}`, code => {
-            if (code === 1) {
-                echo('\nRelease error, check output above.'.red);
-                return;
-            }
+  const npmVersion = version => {
+    exec(`npm version ${version}`, code => {
+      if (code === 1) {
+        echo('\nRelease error, check output above.'.red)
+        return
+      }
 
-            echo('\nsocialshares released!'.green);
-        });
-    };
+      echo('\nsocialshares released!'.green)
+    })
+  };
 
-    if (answers.version === 'custom') {
-        inquirer.prompt([
-            {
-                name: 'version',
-                message: 'Enter the new release version number (MAJOR.MINOR.PATCH):',
-            },
-        ]).then(answers => { npmVersion(answers.version) });
-    } else {
-        npmVersion(answers.version);
-    }
-});
+  if (answers.version === 'custom') {
+    inquirer.prompt([
+      {
+        name: 'version',
+        message: 'Enter the new release version number (MAJOR.MINOR.PATCH):',
+      },
+    ]).then(answers => { npmVersion(answers.version) })
+  } else {
+    npmVersion(answers.version)
+  }
+})
